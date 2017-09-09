7ac8b3392eaee409b59094ececfdcde3b7a140b2
sniperHW <sniperHW@163.com>
2017-09-09 23:41:36 +0800

    tcpserver.go

--- example/tcp_echo.go
+++ example/tcp_echo.go
@@ -13,11 +13,8 @@ func main(){
 		fmt.Printf("usage ./testserver ip:port\n")
 		return
 	}
-
 	service := os.Args[1]
-	
 	server,err := kendynet.NewTcpServer("tcp4",service)
-
 	if server != nil {
 		fmt.Printf("server running on:%s\n",service)
 		err = server.Start(func(session kendynet.StreamSession) {
@@ -42,37 +39,4 @@ func main(){
 	} else {
 		fmt.Printf("NewTcpServer failed %s\n",err)
 	}
-
-
-	/*tcpAddr,err := net.ResolveTCPAddr("tcp4", service)
-	if err != nil{
-		fmt.Printf("ResolveTCPAddr error\n")
-		return
-	}
-	listener, err := net.ListenTCP("tcp", tcpAddr)
-	if err != nil{
-		fmt.Printf("ListenTCP error\n")
-	}
-
-	fmt.Printf("server running on %s\n",service)
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			continue
-		}
-		session := kendynet.NewStreamSocket(conn)
-		session.SetReceiver(protocal_stream_socket.NewBinaryReceiver(4096))
-		session.SetCloseCallBack(func (sess kendynet.StreamSession, reason string) {
-			fmt.Printf("client close:%s\n",reason)
-		})
-		session.SetPacketCallBack(func (sess kendynet.StreamSession,msg interface{},err error) {
-			if nil != err {
-				session.Close("none",0)
-			} else {
-				session.SendBuff(msg.(*kendynet.ByteBuffer))
-			}
-		})
-		session.Start()
-	}*/
 }
