83c40affc38d443482226be5d057e0ae0d510f68
sniperHW <sniperHW@163.com>
2017-09-11 23:32:57 +0800

    none

--- encoder.go
+++ encoder.go
@@ -1,5 +1,13 @@
+
+/*
+ * 编码器接口，根据应用需求实现接口
+*/
+
 package kendynet
 
 type EnCoder interface {
-	EnCode(o interface{}) (*ByteBuffer,error)
+	/*
+	 *  输入一个对象，输出可供session发送的Message对象
+	*/
+	EnCode(o interface{}) (Message,error)
 }
