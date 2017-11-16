45d5e15661cf30546adcb390fa31137979afad42
sniperHW <sniperHW@163.com>
2017-11-17 00:50:21 +0800

    none

--- codec/stream_socket/raw.go
+++ codec/stream_socket/raw.go
@@ -11,43 +11,26 @@ import (
 
 const (
 	minBuffSize = 4096
-	minSizeRemain = 256
 )
 
 type RawReceiver struct {
-	buffsize  uint64
-	space     uint64
 	buffer    [] byte
 }
 
 func (this *RawReceiver) ReceiveAndUnpack(sess kendynet.StreamSession) (interface{},error) {
-	
-	//如果缓冲区小于minSizeRemain字节，重新分配缓冲区
-	if this.space < minSizeRemain {
-		this.buffer = make([]byte,this.buffsize)
-		this.space = this.buffsize
-	}
-
-	idx := this.buffsize - this.space
-
-	n,err := sess.(*stream_socket.StreamSocket).Read(this.buffer[idx:])
+	n,err := sess.(*stream_socket.StreamSocket).Read(this.buffer[:])
 	if err != nil {
 		return nil,err
 	}
-
-	if n <= 0 {
-		return nil,nil
-	}
-
-	this.space -= uint64(n)
-
-	return kendynet.NewByteBuffer(this.buffer[idx:idx+uint64(n)],(uint64)(n)),nil		
+	msg := kendynet.NewByteBuffer(n)
+	msg.AppendBytes(this.buffer[:n])
+	return msg,err	
 }
-
 
 func NewRawReceiver(buffsize uint64)(*RawReceiver){
 	if buffsize < minBuffSize {
 		buffsize = minBuffSize
 	}
-	return &RawReceiver{buffsize:buffsize,space:0}
+	return &RawReceiver{buffer:make([]byte,buffsize)}
 }
+
