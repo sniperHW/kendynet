658b81e7b99a582249e28f7b72d84fd9f2d4d4b5
sniperHW <sniperHW@163.com>
2019-01-19 19:58:57 +0800

    none

--- example/test_rpc/test_rpc.go
+++ example/test_rpc/test_rpc.go
@@ -1,209 +1,205 @@
 package test_rpc
 
 import (
-	"github.com/golang/protobuf/proto"
-	"github.com/sniperHW/kendynet/rpc"
-	"github.com/sniperHW/kendynet/example/testproto"
-	"github.com/sniperHW/kendynet/example/pb"
-	"github.com/sniperHW/kendynet"
-	"github.com/sniperHW/kendynet/socket/stream_socket/tcp"
-	codec "github.com/sniperHW/kendynet/example/codec/stream_socket"
-	"reflect"
 	"fmt"
+	"github.com/golang/protobuf/proto"
+	"github.com/sniperHW/kendynet"
+	codec "github.com/sniperHW/kendynet/example/codec/stream_socket"
+	"github.com/sniperHW/kendynet/example/pb"
+	"github.com/sniperHW/kendynet/example/testproto"
+	"github.com/sniperHW/kendynet/rpc"
+	"github.com/sniperHW/kendynet/socket/stream_socket/tcp"
+	"reflect"
 	"time"
 )
 
-
 type TcpStreamChannel struct {
-	session    kendynet.StreamSession
-	name       string
+	session kendynet.StreamSession
+	name    string
 }
 
 func NewTcpStreamChannel(sess kendynet.StreamSession) *TcpStreamChannel {
-	r := &TcpStreamChannel{session:sess}
-	r.name = sess.RemoteAddr().String() + "<->" + sess.LocalAddr().String() 
+	r := &TcpStreamChannel{session: sess}
+	r.name = sess.RemoteAddr().String() + "<->" + sess.LocalAddr().String()
 	return r
 }
 
-func(this *TcpStreamChannel) SendRequest(message interface {}) error {
+func (this *TcpStreamChannel) SendRequest(message interface{}) error {
 	return this.session.Send(message)
 }
 
-func(this *TcpStreamChannel) SendResponse(message interface {}) error {
+func (this *TcpStreamChannel) SendResponse(message interface{}) error {
 	return this.session.Send(message)
 }
 
-func(this *TcpStreamChannel) Name() string {
+func (this *TcpStreamChannel) Name() string {
 	return this.name
 }
 
-func(this *TcpStreamChannel)	GetSession() kendynet.StreamSession {
+func (this *TcpStreamChannel) GetSession() kendynet.StreamSession {
 	return this.session
 }
-
 
 type TestEncoder struct {
 }
 
-func (this *TestEncoder) Encode(message rpc.RPCMessage) (interface{},error) {
+func (this *TestEncoder) Encode(message rpc.RPCMessage) (interface{}, error) {
 	if message.Type() == rpc.RPC_REQUEST {
 		req := message.(*rpc.RPCRequest)
 		request := &testproto.RPCRequest{
-			Seq : proto.Uint64(req.Seq),
-			Method : proto.String(req.Method),
-			NeedResp : proto.Bool(req.NeedResp),
+			Seq:      proto.Uint64(req.Seq),
+			Method:   proto.String(req.Method),
+			NeedResp: proto.Bool(req.NeedResp),
 		}
 		if req.Arg != nil {
-			buff,err := pb.Encode(req.Arg,1000)
+			buff, err := pb.Encode(req.Arg, 1000)
 			if err != nil {
-				fmt.Printf("encode error: %s\n",err.Error())
-				return nil,err
+				fmt.Printf("encode error: %s\n", err.Error())
+				return nil, err
 			}
 			request.Arg = buff.Bytes()
 		}
-		return request,nil
+		return request, nil
 	} else {
 		resp := message.(*rpc.RPCResponse)
-		response := &testproto.RPCResponse{Seq : proto.Uint64(resp.Seq)}
+		response := &testproto.RPCResponse{Seq: proto.Uint64(resp.Seq)}
 		if resp.Err != nil {
 			response.Err = proto.String(resp.Err.Error())
 		}
 		if resp.Ret != nil {
-			buff,err := pb.Encode(resp.Ret,1000)
+			buff, err := pb.Encode(resp.Ret, 1000)
 			if err != nil {
-				fmt.Printf("encode error: %s\n",err.Error())
-				return nil,err
+				fmt.Printf("encode error: %s\n", err.Error())
+				return nil, err
 			}
 			response.Ret = buff.Bytes()
 		}
-		return response,nil
+		return response, nil
 	}
 }
 
 type TestDecoder struct {
 }
 
-func (this *TestDecoder) Decode(o interface{}) (rpc.RPCMessage,error) {	
+func (this *TestDecoder) Decode(o interface{}) (rpc.RPCMessage, error) {
 	switch o.(type) {
-		case *testproto.RPCRequest:
-			req := o.(*testproto.RPCRequest)
-			request := &rpc.RPCRequest{
-				Seq : req.GetSeq(),
-				Method : req.GetMethod(),
-				NeedResp : req.GetNeedResp(),
+	case *testproto.RPCRequest:
+		req := o.(*testproto.RPCRequest)
+		request := &rpc.RPCRequest{
+			Seq:      req.GetSeq(),
+			Method:   req.GetMethod(),
+			NeedResp: req.GetNeedResp(),
+		}
+		if len(req.Arg) > 0 {
+			var err error
+			request.Arg, _, err = pb.Decode(req.Arg, 0, (uint64)(len(req.Arg)), 1000)
+			if err != nil {
+				return nil, err
 			}
-			if len(req.Arg) > 0 {
-				var err error
-				request.Arg,_,err = pb.Decode(req.Arg,0,(uint64)(len(req.Arg)),1000)
-				if err != nil {
-					return nil,err
-				}
-			}
+		}
 
-			return request,nil
-		case *testproto.RPCResponse:
-			resp := o.(*testproto.RPCResponse)
-			response := &rpc.RPCResponse{Seq : resp.GetSeq()}
-			if resp.Err != nil {
-				response.Err = fmt.Errorf(resp.GetErr())
+		return request, nil
+	case *testproto.RPCResponse:
+		resp := o.(*testproto.RPCResponse)
+		response := &rpc.RPCResponse{Seq: resp.GetSeq()}
+		if resp.Err != nil {
+			response.Err = fmt.Errorf(resp.GetErr())
+		}
+		if len(resp.Ret) > 0 {
+			var err error
+			response.Ret, _, err = pb.Decode(resp.Ret, 0, (uint64)(len(resp.Ret)), 1000)
+			if err != nil {
+				return nil, err
 			}
-			if len(resp.Ret) > 0 {
-				var err error
-				response.Ret,_,err = pb.Decode(resp.Ret,0,(uint64)(len(resp.Ret)),1000)
-				if err != nil {
-					return nil,err
-				}
-			}
-			return response,nil
-		default:
-			return nil,fmt.Errorf("invaild obj type:%s",reflect.TypeOf(o).String())
-	}	
+		}
+		return response, nil
+	default:
+		return nil, fmt.Errorf("invaild obj type:%s", reflect.TypeOf(o).String())
+	}
 }
 
 type RPCServer struct {
-	server     *rpc.RPCServer
-	listener   *tcp.Listener
+	server   *rpc.RPCServer
+	listener *tcp.Listener
 }
 
 func NewRPCServer() *RPCServer {
 	r := &RPCServer{}
-	r.server,_ = rpc.NewRPCServer(&TestDecoder{},&TestEncoder{})
+	r.server, _ = rpc.NewRPCServer(&TestDecoder{}, &TestEncoder{})
 	return r
 }
 
-func (this *RPCServer) RegisterMethod(name string,method rpc.RPCMethodHandler) {
-	this.server.RegisterMethod(name,method)
+func (this *RPCServer) RegisterMethod(name string, method rpc.RPCMethodHandler) {
+	this.server.RegisterMethod(name, method)
 }
 
 func (this *RPCServer) Serve(service string) error {
 	var err error
-	this.listener,err = tcp.NewListener("tcp",service)
+	this.listener, err = tcp.NewListener("tcp", service)
 	if err != nil {
 		return err
 	}
 
-	err = this.listener.Start(func(session kendynet.StreamSession){
+	err = this.listener.Start(func(session kendynet.StreamSession) {
 		channel := NewTcpStreamChannel(session)
 		session.SetEncoder(codec.NewPbEncoder(65535))
 		session.SetReceiver(codec.NewPBReceiver(65535))
-		session.SetRecvTimeout(5 * time.Second)	
-		session.Start(func (event *kendynet.Event) {
+		session.SetRecvTimeout(5 * time.Second)
+		session.Start(func(event *kendynet.Event) {
 			if event.EventType == kendynet.EventTypeError {
-				session.Close(event.Data.(error).Error(),0)
+				session.Close(event.Data.(error).Error(), 0)
 			} else {
-				this.server.OnRPCMessage(channel,event.Data)
+				this.server.OnRPCMessage(channel, event.Data)
 			}
 		})
 	})
 	return err
 }
 
-
 type Caller struct {
-	client     *rpc.RPCClient
+	client *rpc.RPCClient
 }
 
 func NewCaller() *Caller {
 	return &Caller{}
 }
 
-func (this *Caller) Dial(service string,timeout time.Duration) error {
-	connector,err := tcp.NewConnector("tcp",service)
-	session,err := connector.Dial(timeout)
+func (this *Caller) Dial(service string, timeout time.Duration) error {
+	connector, err := tcp.NewConnector("tcp", service)
+	session, err := connector.Dial(timeout)
 	if err != nil {
 		return err
-	} 
+	}
 	channel := NewTcpStreamChannel(session)
-	this.client,_ = rpc.NewClient(channel,&TestDecoder{},&TestEncoder{})
+	this.client, _ = rpc.NewClient(channel, &TestDecoder{}, &TestEncoder{})
 	session.SetEncoder(codec.NewPbEncoder(65535))
 	session.SetReceiver(codec.NewPBReceiver(65535))
-	session.SetRecvTimeout(5 * time.Second)	
-	session.SetCloseCallBack(func (sess kendynet.StreamSession, reason string) {
+	session.SetRecvTimeout(5 * time.Second)
+	session.SetCloseCallBack(func(sess kendynet.StreamSession, reason string) {
 		this.client.OnChannelClose(fmt.Errorf(reason))
-		fmt.Printf("channel close:%s\n",reason)
+		fmt.Printf("channel close:%s\n", reason)
 	})
-	session.Start(func (event *kendynet.Event) {
+	session.Start(func(event *kendynet.Event) {
 		if event.EventType == kendynet.EventTypeError {
-			session.Close(event.Data.(error).Error(),0)
+			session.Close(event.Data.(error).Error(), 0)
 		} else {
-			this.client.OnRPCMessage(event.Data)	
+			this.client.OnRPCMessage(event.Data)
 		}
 	})
 	return nil
 }
 
-func (this *Caller) AsynCall(method string,arg interface{},timeout uint32,cb rpc.RPCResponseHandler) error {
-	return this.client.AsynCall(method,arg,timeout,cb)
+func (this *Caller) AsynCall(method string, arg interface{}, timeout uint32, cb rpc.RPCResponseHandler) {
+	this.client.AsynCall(method, arg, timeout, cb)
 }
 
-func (this *Caller) SyncCall(method string,arg interface{},timeout uint32) (interface{},error) {
-	return this.client.SyncCall(method,arg,timeout)
+func (this *Caller) SyncCall(method string, arg interface{}, timeout uint32) (interface{}, error) {
+	return this.client.SyncCall(method, arg, timeout)
 }
 
 func init() {
-	pb.Register(&testproto.Hello{},1)
-	pb.Register(&testproto.World{},2)
-	pb.Register(&testproto.RPCResponse{},3)
-	pb.Register(&testproto.RPCRequest{},4)
+	pb.Register(&testproto.Hello{}, 1)
+	pb.Register(&testproto.World{}, 2)
+	pb.Register(&testproto.RPCResponse{}, 3)
+	pb.Register(&testproto.RPCRequest{}, 4)
 }
-
--- rpc/client.go
+++ rpc/client.go
@@ -5,7 +5,7 @@ import (
 	"github.com/sniperHW/kendynet"
 	"github.com/sniperHW/kendynet/event"
 	"github.com/sniperHW/kendynet/util"
-	"os"
+	//"os"
 	"runtime"
 	"sync"
 	"sync/atomic"
@@ -37,9 +37,244 @@ func (this *reqContext) SetIndex(idx uint32) {
 }
 
 func (this *reqContext) callResponseCB(ret interface{}, err error) {
-	pCallOnResponse(this.onResponse, ret, err)
+	defer func() {
+		if r := recover(); r != nil {
+			buf := make([]byte, 65535)
+			l := runtime.Stack(buf, false)
+			kendynet.Errorf(util.FormatFileLine("%s\n", fmt.Sprintf("%v: %s", r, buf[:l])))
+		}
+	}()
+	this.onResponse(ret, err)
 }
 
+type RPCClient struct {
+	encoder      RPCMessageEncoder
+	decoder      RPCMessageDecoder
+	sequence     uint64
+	channel      RPCChannel
+	mtx          sync.Mutex
+	minheap      *util.MinHeap
+	waitResp     map[uint64]*reqContext //待响应的请求
+	cbEventQueue *event.EventQueue
+}
+
+func (this *RPCClient) checkTimeout() {
+	now := time.Now()
+	timeout := []*reqContext{}
+	this.mtx.Lock()
+	for {
+		r := this.minheap.Min()
+		if r != nil && now.After(r.(*reqContext).deadline) {
+			this.minheap.PopMin()
+			if _, ok := this.waitResp[r.(*reqContext).seq]; !ok {
+				kendynet.Infof("timeout context:%d not found\n", r.(*reqContext).seq)
+			} else {
+				delete(this.waitResp, r.(*reqContext).seq)
+				timeout = append(timeout, r.(*reqContext))
+				kendynet.Infof("timeout context:%d\n", r.(*reqContext).seq)
+			}
+		} else {
+			break
+		}
+	}
+	this.mtx.Unlock()
+
+	for _, v := range timeout {
+		if this.cbEventQueue != nil {
+			this.cbEventQueue.PostNoWait(func() {
+				v.callResponseCB(nil, ErrCallTimeout)
+			})
+		} else {
+			v.callResponseCB(nil, ErrCallTimeout)
+		}
+	}
+}
+
+//通道关闭后调用
+func (this *RPCClient) OnChannelClose(err error) {
+	var waitRespBack map[uint64]*reqContext
+
+	this.mtx.Lock()
+	waitRespBack = this.waitResp
+	this.waitResp = map[uint64]*reqContext{}
+	this.minheap.Clear()
+	this.mtx.Unlock()
+
+	for _, v := range waitRespBack {
+		if this.cbEventQueue != nil {
+			this.cbEventQueue.PostNoWait(func() {
+				v.callResponseCB(nil, err)
+			})
+		} else {
+			v.callResponseCB(nil, err)
+		}
+	}
+}
+
+//收到RPC消息后调用
+func (this *RPCClient) OnRPCMessage(message interface{}) {
+	msg, err := this.decoder.Decode(message)
+	if nil != err {
+		kendynet.Errorf(util.FormatFileLine("RPCClient rpc message from(%s) decode err:%s\n", this.channel.Name, err.Error()))
+		return
+	}
+
+	var (
+		ctx *reqContext
+		ok  bool
+	)
+
+	this.mtx.Lock()
+	if ctx, ok = this.waitResp[msg.GetSeq()]; ok {
+		delete(this.waitResp, msg.GetSeq())
+		this.minheap.Remove(ctx)
+	}
+	this.mtx.Unlock()
+
+	if nil != ctx {
+		resp := msg.(*RPCResponse)
+		if this.cbEventQueue != nil {
+			this.cbEventQueue.PostNoWait(func() {
+				ctx.callResponseCB(resp.Ret, resp.Err)
+			})
+		} else {
+			ctx.callResponseCB(resp.Ret, resp.Err)
+		}
+	} else {
+		kendynet.Debugf("on response,but missing reqContext:%d\n", msg.GetSeq())
+	}
+}
+
+//投递，不关心响应和是否失败
+func (this *RPCClient) Post(method string, arg interface{}) error {
+
+	req := &RPCRequest{
+		Method:   method,
+		Seq:      atomic.AddUint64(&this.sequence, 1),
+		Arg:      arg,
+		NeedResp: false,
+	}
+
+	request, err := this.encoder.Encode(req)
+	if err != nil {
+		return fmt.Errorf("encode error:%s\n", err.Error())
+	}
+
+	err = this.channel.SendRequest(request)
+	if nil != err {
+		return err
+	}
+	return nil
+}
+
+/*
+ *  异步调用
+ */
+
+func (this *RPCClient) AsynCall(method string, arg interface{}, timeout uint32, cb RPCResponseHandler) {
+
+	if cb == nil {
+		panic("cb == nil")
+	}
+
+	req := &RPCRequest{
+		Method:   method,
+		Seq:      atomic.AddUint64(&this.sequence, 1),
+		Arg:      arg,
+		NeedResp: true,
+	}
+
+	if timeout <= 0 {
+		timeout = 5000
+	}
+
+	context := &reqContext{
+		onResponse: cb,
+		seq:        req.Seq,
+	}
+
+	request, err := this.encoder.Encode(req)
+	if err != nil {
+		if this.cbEventQueue != nil {
+			this.cbEventQueue.PostNoWait(func() {
+				context.callResponseCB(nil, fmt.Errorf("encode error:%s\n", err.Error()))
+			})
+		} else {
+			context.callResponseCB(nil, fmt.Errorf("encode error:%s\n", err.Error()))
+		}
+	} else {
+
+		context.deadline = time.Now().Add(time.Duration(timeout) * time.Millisecond)
+		context.timestamp = time.Now().UnixNano()
+
+		this.mtx.Lock()
+		this.waitResp[context.seq] = context
+		this.minheap.Insert(context)
+		this.mtx.Unlock()
+
+		err := this.channel.SendRequest(request)
+
+		if nil != err {
+			this.mtx.Lock()
+			delete(this.waitResp, context.seq)
+			this.minheap.Remove(context)
+			this.mtx.Unlock()
+			if this.cbEventQueue != nil {
+				this.cbEventQueue.PostNoWait(func() {
+					context.callResponseCB(nil, err)
+				})
+			} else {
+				context.callResponseCB(nil, err)
+			}
+		}
+	}
+}
+
+//同步调用
+func (this *RPCClient) SyncCall(method string, arg interface{}, timeout uint32) (ret interface{}, err error) {
+	respChan := make(chan interface{})
+	f := func(ret_ interface{}, err_ error) {
+		ret = ret_
+		err = err_
+		respChan <- nil
+	}
+	this.AsynCall(method, arg, timeout, f)
+	_ = <-respChan
+	return
+}
+
+func NewClient(channel RPCChannel, decoder RPCMessageDecoder, encoder RPCMessageEncoder, cbEventQueue ...*event.EventQueue) (*RPCClient, error) {
+	if nil == decoder {
+		return nil, fmt.Errorf("decoder == nil")
+	}
+
+	if nil == encoder {
+		return nil, fmt.Errorf("encoder == nil")
+	}
+
+	if nil == channel {
+		return nil, fmt.Errorf("channel == nil")
+	}
+
+	var q *event.EventQueue
+
+	if len(cbEventQueue) > 0 {
+		q = cbEventQueue[0]
+	}
+	//mtx          sync.Mutex
+	//minheap      *util.MinHeap
+	//waitResp     map[uint64]*reqContext //待响应的请求
+	return &RPCClient{
+		encoder:      encoder,
+		decoder:      decoder,
+		channel:      channel,
+		cbEventQueue: q,
+		minheap:      util.NewMinHeap(65535),
+		waitResp:     map[uint64]*reqContext{},
+	}, nil
+}
+
+/*
 func pCallOnResponse(onResponse RPCResponseHandler, ret interface{}, err error) {
 	defer func() {
 		if r := recover(); r != nil {
@@ -192,7 +427,7 @@ func AsynHandler(cb RPCResponseHandler) RPCResponseHandler {
 /*
 *  异步调用
 *  cb将在一个单独的go程中执行,如需在cb中调用阻塞函数请使用AsynHandler封装cb
- */
+* /
 func (this *RPCClient) AsynCall(method string, arg interface{}, timeout uint32, cb RPCResponseHandler) error {
 
 	if cb == nil {
@@ -289,3 +524,4 @@ func InitClient(queue *event.EventQueue) {
 		}()
 	})
 }
+*/
