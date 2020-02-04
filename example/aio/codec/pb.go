d565d9c1de774aad38795869146f85ec1c26e39b
sniperHW <sniperHW@163.com>
2020-02-04 21:20:29 +0800

    none

--- example/aio/codec/pb.go
+++ example/aio/codec/pb.go
@@ -43,6 +43,75 @@ func (p *BufferPool) Put(buff []byte) {
 }
 
 type PBReceiver struct {
+	buffer         []byte
+	recvBuff       []byte
+	maxpacket      int
+	unpackSize     int
+	unpackIdx      int
+	initBuffSize   int
+	totalMaxPacket int
+	minBuffRemain  int
+}
+
+func NewPBReceiver(pool *BufferPool, maxMsgSize int) *PBReceiver {
+	receiver := &PBReceiver{}
+	//完整数据包大小为head+data
+	receiver.totalMaxPacket = maxMsgSize + int(pb.PBHeaderSize)
+	doubleTotalPacketSize := receiver.totalMaxPacket * 2
+	if doubleTotalPacketSize < minBuffSize {
+		receiver.initBuffSize = minBuffSize
+	} else {
+		receiver.initBuffSize = doubleTotalPacketSize
+	}
+	receiver.buffer = make([]byte, receiver.initBuffSize)
+	receiver.recvBuff = receiver.buffer
+	receiver.maxpacket = maxMsgSize
+	receiver.minBuffRemain = receiver.totalMaxPacket / 4
+	return receiver
+}
+func (this *PBReceiver) unPack() (interface{}, error) {
+	msg, dataLen, err := pb.Decode(this.buffer, uint64(this.unpackIdx), uint64(this.unpackIdx+this.unpackSize), uint64(this.maxpacket))
+	if dataLen > 0 {
+		this.unpackIdx += int(dataLen)
+		this.unpackSize -= int(dataLen)
+	}
+	return msg, err
+}
+
+func (this *PBReceiver) OnRecvOk(s kendynet.StreamSession, buff []byte) {
+	l := len(buff)
+	this.unpackSize += l
+	this.recvBuff = this.recvBuff[l:]
+}
+
+func (this *PBReceiver) ReceiveAndUnpack(s kendynet.StreamSession) (interface{}, error) {
+	msg, err := this.unPack()
+	if nil == msg && nil == err {
+		if len(this.recvBuff) < this.minBuffRemain {
+			if this.unpackSize > 0 {
+				//有数据尚未解包，需要移动到buffer前部
+				copy(this.buffer, this.buffer[this.unpackIdx:this.unpackIdx+this.unpackSize])
+			}
+			this.unpackIdx = 0
+			this.recvBuff = this.buffer[this.unpackSize:]
+		} else if this.unpackSize == 0 {
+			this.unpackIdx = 0
+			this.recvBuff = this.buffer
+		}
+		s.(*aio.AioSocket).PostRecv(this.recvBuff)
+	}
+	return msg, err
+}
+
+func (this *PBReceiver) StartReceive(s kendynet.StreamSession) {
+	s.(*aio.AioSocket).PostRecv(this.recvBuff)
+}
+
+func (this *PBReceiver) OnClose() {
+
+}
+
+/*type PBReceiver struct {
 	buffer       []byte
 	isPoolBuffer bool
 	pool         *BufferPool
@@ -158,4 +227,4 @@ func (this *PBReceiver) ReceiveAndUnpack(s kendynet.StreamSession) (interface{},
 
 		}
 	}
-}
+}*/
