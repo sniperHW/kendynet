c1f3c15ce5395ced9a24a25f404664e1a66e100f
huangwei <sniperhw@163.com>
2021-03-18 14:38:38 +0800

    none

--- buffer/buffer.go
+++ buffer/buffer.go
@@ -2,8 +2,11 @@ package buffer
 
 import (
 	"encoding/binary"
+	"errors"
 	"unsafe"
 )
+
+var ErrBeyondSize error = errors.New("beyond size")
 
 type Buffer struct {
 	bs   []byte
@@ -177,6 +180,16 @@ func (this *BufferReader) GetByte() byte {
 	}
 }
 
+func (this *BufferReader) CheckGetByte() (byte, error) {
+	if this.offset+1 > len(this.bs) {
+		return 0, ErrBeyondSize
+	} else {
+		ret := this.bs[this.offset]
+		this.offset += 1
+		return ret, nil
+	}
+}
+
 func (this *BufferReader) GetUint16() uint16 {
 	if this.offset+2 > len(this.bs) {
 		return 0
@@ -187,8 +200,27 @@ func (this *BufferReader) GetUint16() uint16 {
 	}
 }
 
+func (this *BufferReader) CheckGetUint16() (uint16, error) {
+	if this.offset+2 > len(this.bs) {
+		return 0, ErrBeyondSize
+	} else {
+		ret := binary.BigEndian.Uint16(this.bs[this.offset : this.offset+2])
+		this.offset += 2
+		return ret, nil
+	}
+}
+
 func (this *BufferReader) GetInt16() int16 {
 	return int16(this.GetUint16())
+}
+
+func (this *BufferReader) CheckGetInt16() (int16, error) {
+	u, err := this.CheckGetUint16()
+	if nil != err {
+		return 0, err
+	} else {
+		return int16(u), nil
+	}
 }
 
 func (this *BufferReader) GetUint32() uint32 {
@@ -201,8 +233,27 @@ func (this *BufferReader) GetUint32() uint32 {
 	}
 }
 
+func (this *BufferReader) CheckGetUint32() (uint32, error) {
+	if this.offset+4 > len(this.bs) {
+		return 0, ErrBeyondSize
+	} else {
+		ret := binary.BigEndian.Uint32(this.bs[this.offset : this.offset+4])
+		this.offset += 4
+		return ret, nil
+	}
+}
+
 func (this *BufferReader) GetInt32() int32 {
 	return int32(this.GetUint32())
+}
+
+func (this *BufferReader) CheckGetInt32() (int32, error) {
+	u, err := this.CheckGetUint32()
+	if nil != err {
+		return 0, err
+	} else {
+		return int32(u), nil
+	}
 }
 
 func (this *BufferReader) GetUint64() uint64 {
@@ -215,12 +266,40 @@ func (this *BufferReader) GetUint64() uint64 {
 	}
 }
 
+func (this *BufferReader) CheckGetUint64() (uint64, error) {
+	if this.offset+8 > len(this.bs) {
+		return 0, ErrBeyondSize
+	} else {
+		ret := binary.BigEndian.Uint64(this.bs[this.offset : this.offset+8])
+		this.offset += 8
+		return ret, nil
+	}
+}
+
 func (this *BufferReader) GetInt64() int64 {
 	return int64(this.GetUint64())
 }
 
+func (this *BufferReader) CheckGetInt64() (int64, error) {
+	u, err := this.CheckGetUint64()
+	if nil != err {
+		return 0, err
+	} else {
+		return int64(u), nil
+	}
+}
+
 func (this *BufferReader) GetString(size int) string {
 	return string(this.GetBytes(size))
+}
+
+func (this *BufferReader) CheckGetString(size int) (string, error) {
+	b, err := this.CheckGetBytes(size)
+	if nil != err {
+		return "", err
+	} else {
+		return string(b), nil
+	}
 }
 
 func (this *BufferReader) GetBytes(size int) []byte {
@@ -230,4 +309,13 @@ func (this *BufferReader) GetBytes(size int) []byte {
 	ret := this.bs[this.offset : this.offset+size]
 	this.offset += size
 	return ret
+}
+
+func (this *BufferReader) CheckGetBytes(size int) ([]byte, error) {
+	if len(this.bs)-this.offset < size {
+		return nil, ErrBeyondSize
+	}
+	ret := this.bs[this.offset : this.offset+size]
+	this.offset += size
+	return ret, nil
 }
