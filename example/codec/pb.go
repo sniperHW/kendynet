9bab6123eb2c018581adcbba48a92c3b40a4f4ce
huangwei <sniperhw@163.com>
2021-04-23 09:10:08 +0800

    none

--- example/codec/pb.go
+++ example/codec/pb.go
@@ -2,13 +2,28 @@ package codec
 
 import (
 	"errors"
-	"github.com/sniperHW/kendynet"
+	//"github.com/sniperHW/kendynet"
 	"github.com/sniperHW/kendynet/buffer"
 	"github.com/sniperHW/kendynet/example/pb"
-	"github.com/sniperHW/kendynet/socket"
+	//"github.com/sniperHW/kendynet/socket"
 )
 
-const minBuffSize = 64
+func isPow2(size int) bool {
+	return (size & (size - 1)) == 0
+}
+
+func sizeofPow2(size int) int {
+	if isPow2(size) {
+		return size
+	}
+	size = size - 1
+	size = size | (size >> 1)
+	size = size | (size >> 2)
+	size = size | (size >> 4)
+	size = size | (size >> 8)
+	size = size | (size >> 16)
+	return size + 1
+}
 
 type PbEncoder struct {
 	maxMsgSize int
@@ -28,6 +43,59 @@ func (this *PbEncoder) EnCode(o interface{}, b *buffer.Buffer) (e error) {
 }
 
 type PBReceiver struct {
+	buffer    []byte
+	w         int
+	r         int
+	maxpacket int
+}
+
+func NewPBReceiver(maxMsgSize int) *PBReceiver {
+	return &PBReceiver{
+		buffer:    make([]byte, 4096),
+		maxpacket: maxMsgSize,
+	}
+}
+
+func (this *PBReceiver) GetRecvBuff() []byte {
+	return this.buffer[this.w:]
+}
+
+func (this *PBReceiver) OnData(data []byte) {
+	this.w += len(data)
+}
+
+func (this *PBReceiver) Unpack() (interface{}, error) {
+	if this.r == this.w {
+		return nil, nil
+	} else {
+		msg, packetSize, err := pb.Decode(this.buffer, this.r, this.w, this.maxpacket)
+		if nil != msg {
+			this.r += packetSize
+			if this.r == this.w {
+				this.r = 0
+				this.w = 0
+			}
+		} else if nil == err {
+			if packetSize > cap(this.buffer) {
+				buffer := make([]byte, sizeofPow2(packetSize))
+				copy(buffer, this.buffer[this.r:this.w])
+				this.buffer = buffer
+			} else {
+				//空间足够容纳下一个包，
+				copy(this.buffer, this.buffer[this.r:this.w])
+			}
+			this.w = this.w - this.r
+			this.r = 0
+		}
+		return msg, err
+	}
+}
+
+func (this *PBReceiver) OnSocketClose() {
+
+}
+
+/*type PBReceiver struct {
 	recvBuff       []byte
 	buffer         []byte
 	maxpacket      int
@@ -148,4 +216,4 @@ func (this *PBReceiver) Unpack() (msg interface{}, err error) {
 
 func (this *PBReceiver) OnSocketClose() {
 
-}
+}*/
